671962751f304e8433bfc988cdd596e731569670
BasantiNagari <basantinagari23@gmail.com>
2023-09-16 14:46:21 +0545

    dropdown selector in react

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const countries = [
+  {
+    name: "Nepal",
+    value: "np",
+    cities: ["Kathmandu", "Bhaktapur", "Lalitpur"],
+  },
+  {
+    name: "India",
+    value: "in",
+    cities: ["Banglore", "New Delhi", "Mumbi"],
+  },
+  {
+    name: "Pakistan",
+    value: "PK",
+    cities: ["Karachi", "Lahore", "Islamabad"],
+  },
+];
+
+function App() {
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  return (
+    <>
+      <select
+        onChange={(e) => {
+          setSelectedCountry(e.target.value);
+        }}
+      >
+        <option value=""> Select a Country...</option>
+
+        {countries &&
+          countries.map((country, index) => {
+            return (
+              <option key={index} value={country?.value}>
+                {country?.name}
+              </option>
+            );
+          })}
+      </select>
+
+      {selectedCountry && (
+        <>
+          <select
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          >
+            <option value=""> Select a City...</option>
+            {countries &&
+              countries
+                .find((country) => country.value === selectedCountry)
+                ?.cities.map((city, index) => {
+                  return (
+                    <option key={index} value={city}>
+                      {city}
+                    </option>
+                  );
+                })}
+          </select>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
+
+/*create a dropdown  using countries name
+  based on the country name selected,
+  show hide, another select dropdown based on their cities*/
